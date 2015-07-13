f7ccc2ca46db5081b7e8144eca3bcc5249f63820
Mithos-Yggdrasill <pieter_declercq@hotmail.com>
2015-07-13 16:35:54 +0200

    Implemented AnswerRepository (nominal)

--- app/Model/Entities/Residence.php
+++ app/Model/Entities/Residence.php
@@ -10,79 +10,78 @@ namespace App\Model\Entities;
  * @author Pieter - #oSoc15
  */
 class Residence extends \Identifier {
-    
+
     /**
      * The longitude coordinate of the residence.
      * @var Integer The longitude coordinate of the residence 
      */
     private $_longitude;
-    
+
     /**
      * The latitude coordinate of the residence.
      * @var Integer The latitude coordinate of the residence 
      */
     private $_latitude;
-    
+
     /**
      * The info supplied about this residence stored in a associative array with
      * the questionId as key and the answerId as value.
      * @var array   The given input for each question. 
      */
     private $_info;
-    
+
     public function __construct($longitude = 0, $latitude = 0, $info = array()) {
         $this->_latitude = $latitude;
         $this->_longitude = $longitude;
         $this->_info = $info;
     }
-    
+
     /**
      * The getter for the longitude of the residence
      * @return Integer The longitude of the residence
      */
-    public function getLongitude(){
+    public function getLongitude() {
         return $this->_longitude;
     }
-    
+
     /**
      * The setter for the longitude of the residence.
      * @param Integer $longitude The longitude of the residence
      */
-    public function setLongitude($longitude){
+    public function setLongitude($longitude) {
         $this->_longitude = $longitude;
     }
-    
+
     /**
      * The getter for the latitude of the residence.
      * @return Integer $latitude The latitude of the residence
      */
-    public function getLatitude(){
+    public function getLatitude() {
         return $this->_latitude;
     }
-    
+
     /**
      * The setter for the latitude of the residence.
      * @param Integer $latitude The latitude of the residence
      */
-    public function setLatitude($latitude){
+    public function setLatitude($latitude) {
         $this->_latitude = $latitude;
     }
-    
+
     /**
      * A method which returns all the answered question about this residence.
      * @return array with the questionId as key and the answerId as value.
      */
-    public function getAllInfo(){
+    public function getAllInfo() {
         return $this->_info;
     }
-    
+
     /**
      * A method which returns the answer given to a given question.
      * @param Integer $questionId The id of the question you want
      */
-    public function getInfo($questionId){
+    public function getInfo($questionId) {
         return $this->_info[$questionId];
     }
-    
-    
+
 }
--- app/Model/Repositories/AnswerRepository.php
+++ app/Model/Repositories/AnswerRepository.php
@@ -8,31 +8,66 @@
 class AnswerRepository implements IRepostitory {
 
     /**
+     * An associative array which represents the repository. The key is the id 
+     * of the answer and the value is the actual answerobject.
+     * @var array
+     */
+    private $_answers;
+
+    /**
+     * The private constructor according to the Singleton-pattern.
+     */
+    private function __construct() {
+        $this->_answers = array();
+    }
+
+    /**
+     * Private clone method to prevent cloning of the instance of the
+     * *Singleton* instance.
+     *
+     * @return void
+     */
+    private function __clone() {}
+
+    /**
+     * The only way to get the repository.
+     * @staticvar type $inst The repository for the answers
+     * @return \AnswerRepository
+     */
+    public static function getInstance() {
+        static $inst = null;
+        if ($inst === null) {
+            $inst = new AnswerRepository();
+        }
+        return $inst;
+    }
+
+    /**
      * The method to add an answer to the repository.
      */
     public function add($answer) {
-        //TODO
+        $this->_answers[$answer->getId()] = $answer;
     }
 
     /**
      * The method to get an answer from the repository.
      */
     public function get($answerId) {
-        //TODO
+        return $this->_answers[$answerId];
     }
 
     /**
      * The method to get all the answers from the repository.
      */
     public function getAll() {
-        //TODO
+        return $this->_answers;
     }
 
     /**
      * The method to delete a answer from the repository. 
      */
     public function delete($answerId) {
-        //TODO
+        unset($this->_answers[$answerId]);
     }
 
 }
