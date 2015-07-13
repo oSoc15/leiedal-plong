e67d1809aa6f1113ecacb82ce1851770d1c29739
Mithos-Yggdrasill <pieter_declercq@hotmail.com>
2015-07-13 15:41:29 +0200

    Question + Answer implementation

--- app/Model/Entities/Answer.php
+++ app/Model/Entities/Answer.php
@@ -2,8 +2,65 @@
 
 namespace App;
 
+/**
+ * This class is the respresentation of a answer to a certain question. It
+ * has a value to which this answer is accounted for in the algorithm to 
+ * determine the ecolabel.
+ *
+ * @author Pieter - #oSoc15
+ */
+class Answer extends \Identifier {
 
-class Answer extends \Identifier
-{
-    //
+    /**
+     *  The answer to be displayed to the user.
+     */
+    private $_answer;
+
+    /**
+     * The possible question which will follow up on this answer.
+     * @var Question the question which goes further upon this answer.
+     */
+    private $_subquestion;
+
+    /**
+     * The constructor for an Answer
+     * @param String $answer The sentence which is the answer
+     */
+    public function __construct($answer = 'BEST ANSWER EVER') {
+        $this->_answer = $answer;
+    }
+
+    /**
+     * The setter for the anwer
+     * @param The sentence of the answer
+     */
+    public function setAnswer($answer) {
+        $this->_answer = $answer;
+    }
+
+    /**
+     * The getter for the answer.
+     * @return The sentence of the answer
+     */
+    public function getAnswer() {
+        return $this->_answer;
+    }
+
+    /**
+     * The getter for the subquestion of this answer.
+     * @return Question The subquestion which follows up on this answer.
+     */
+    public function get_subquestion() {
+        return $this->_subquestion;
+    }
+
+    /**
+     * The setter for the subquestion of this answer.
+     * 
+     * @param Question $_subquestion The subquestion which follows up onto this answer.
+     */
+    public function set_subquestion($_subquestion) {
+        $this->_subquestion = $_subquestion;
+    }
+
 }
--- app/Model/Entities/Question.php
+++ app/Model/Entities/Question.php
@@ -2,7 +2,133 @@
 
 namespace App;
 
-class Question extends \Identifier
-{
-    //
+/**
+ * This class is the respresentation of a question which can be asked to 
+ * determine the label of the house in the questionnairy. It has the weight in 
+ * which this question's answer is accounted for in the algorithm to determine 
+ * the ecolabel. It has an array of possible answers to this question.
+ *
+ * @author Pieter - #oSoc15
+ */
+class Question extends Identifier {
+    /*
+     * The sentence displayed to the user.
+     */
+
+    private $_question;
+
+    /**
+     * The String representation of the question which holds all the information this question posseses.
+     * @var String The extended version of the question. 
+     */
+    private $_extendedQuestion;
+
+    /**
+     * An array with all the possible answer to this question.
+     */
+    private $_answer;
+
+    /**
+     * The type of the question.
+     * @var QuestionTypes
+     */
+    private $_type;
+
+    /**
+     * Can you give an input in this question.
+     * @var boolean
+     */
+    private $_input;
+
+    /**
+     * A constructor which creates a question with it's possible answers.
+     * @param String $question    The sentence to be displayed to the user
+     * @param array(Answer) $answer  The array of possible answers
+     */
+    public function __construct($question = 'does this work?', $extendedQuestion = "", $input = false, $answer = array('yes', 'no')) {
+        $this->_question = $question;
+        $this->_answer = $answer;
+        $this->_extendedQuestion = $extendedQuestion;
+        $this->_input = $input;
+    }
+
+    /**
+     * The getter for the sentence of the question.
+     * @return The sentence to be asked.
+     */
+    public function getQuestion() {
+        return $this->_question;
+    }
+
+    /**
+     * The setter for the sentence of the question.
+     * @param String $question The short String representation of the question.
+     */
+    public function setQuestion($question) {
+        $this->_question = $question;
+    }
+
+    /**
+     * The getter for the extended version of the question.
+     * @return The extended version of the question
+     */
+    public function getExtendedQuestion() {
+        return $this->_extendedQuestion;
+    }
+
+    /**
+     * The setter for the extended version of the question.
+     * @param String $_extendedQuestion The extended version of the question.
+     */
+    public function setExtendedQuestion($_extendedQuestion) {
+        $this->_extendedQuestion = $_extendedQuestion;
+    }
+
+    /**
+     * The getter for the answer of the question.
+     * @return The array of possible answers to this question.
+     */
+    public function getAnswer() {
+        return $this->_answer;
+    }
+
+    /**
+     * The setter for the answer of the question.
+     * @param array(Answer) $answer The possible answer to this question.
+     */
+    public function setAnswer($answer) {
+        $this->_answer = $answer;
+    }
+
+    /**
+     * The getter for the type of the question.
+     * @return QuestionTypes The type of the question
+     */
+    public function get_type() {
+        return $this->_type;
+    }
+
+    /**
+     * The setter for the type of the question.
+     * @return QuestionTypes The type of the question
+     */
+    public function setType($_type) {
+        $this->_type = $_type;
+    }
+
+    /**
+     * The getter which defines if the question has an inputtype.
+     * @return boolean  True if you have an inputfield. False otherwise.
+     */
+    public function hasInput() {
+        return $this->_input;
+    }
+
+    /**
+     * The setter which defines if the question has an inputtype.
+     * @param $_input True if you have an inputfield. False otherwise.
+     */
+    public function setInput($_input) {
+        $this->_input = $_input;
+    }
 }
