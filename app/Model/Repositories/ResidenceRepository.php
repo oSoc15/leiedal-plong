244274c43ff8b2ac0ee0dda495a9cd834eb7e707
Mithos-Yggdrasill <pieter_declercq@hotmail.com>
2015-07-13 16:43:49 +0200

    Implemented ResidenceRepository (nominal)

--- app/Model/Repositories/ResidenceRepository.php
+++ app/Model/Repositories/ResidenceRepository.php
@@ -8,31 +8,66 @@
 class ResidenceRepository implements IRepository {
 
     /**
+     * An associative array which represents the repository. The key is the id 
+     * of the residence and the value is the actual residencerobject.
+     * @var array
+     */
+    private $_residences;
+
+    /**
+     * The private constructor according to the Singleton-pattern.
+     */
+    private function __construct() {
+        $this->_residences = array();
+    }
+
+    /**
+     * Private clone method to prevent cloning of the instance of the
+     * *Singleton* instance.
+     *
+     * @return void
+     */
+    private function __clone() {}
+
+    /**
+     * The only way to get the repository.
+     * @staticvar type $inst The repository for the residences
+     * @return \ResidenceRepository
+     */
+    public static function getInstance() {
+        static $inst = null;
+        if ($inst === null) {
+            $inst = new ResidenceRepository();
+        }
+        return $inst;
+    }
+    
+    /**
      * The method to add an residence to the repository.
      */
     public function add($residence) {
-        //TODO
+        $this->_residences[$residence->getId()] = $residence;
     }
 
     /**
      * The method to get an residence from the repository.
      */
-    public function get($residence) {
-        //TODO
+    public function get($residenceId) {
+        return $this->_residences[$residenceId];
     }
 
     /**
      * The method to get all the residences from the repository.
      */
     public function getAll() {
-        //TODO
+        return $this->_residences;
     }
 
     /**
      * The method to delete a residence from the repository. 
      */
     public function delete($residenceId) {
-        //TODO
+        unset($this->_residences[$residenceId]);
     }
 
 }
