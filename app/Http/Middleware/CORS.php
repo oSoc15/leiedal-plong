666632359ae22e4dbb8468bc05559272c7281c4f
michieldewilde <mich.dewild@gmail.com>
2015-07-17 14:59:50 +0200

    Add middleware CORS

---
+++ app/Http/Middleware/CORS.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\Http\Middleware;
+
+use Closure;
+
+class CORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $headers = [
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+        'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+        ];
+
+        if ($request->getMethod() == 'OPTIONS') {
+            $response = new Response();
+            foreach ($headers as $key => $value) {
+                $response->headers->set($key, $value);
+            }
+
+            return $response;
+        }
+
+        $response = $next($request);
+
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+
+        return $response;
+    }
+}
