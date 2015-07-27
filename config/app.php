8ceb019f0d498599c4b3bf0b457e7f0d3bd6a65a
michieldewilde <mich.dewild@gmail.com>
2015-07-27 16:25:40 +0200

    Removed unused dependencies

--- config/app.php
+++ config/app.php
@@ -137,7 +137,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Illuminate\Html\HtmlServiceProvider::class,
-        Laracasts\Generators\GeneratorsServiceProvider::class,
         Vinkla\Hashids\HashidsServiceProvider::class,
 
         /*
